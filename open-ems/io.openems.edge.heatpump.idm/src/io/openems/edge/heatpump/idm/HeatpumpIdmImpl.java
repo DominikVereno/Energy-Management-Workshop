bda29e03873e8871ca25e174bbd725839d432306
DominikVereno <dominik.vereno@fh-salzburg.ac.at>
2026-09-21 10:38:31 +0200

    Registers

--- open-ems/io.openems.edge.heatpump.idm/src/io/openems/edge/heatpump/idm/HeatpumpIdmImpl.java
+++ open-ems/io.openems.edge.heatpump.idm/src/io/openems/edge/heatpump/idm/HeatpumpIdmImpl.java
@@ -108,6 +108,10 @@ public class HeatpumpIdmImpl extends AbstractOpenemsModbusComponent
 						m(HeatpumpIdm.ChannelId.HEAT_DEMAND,
 								new UnsignedWordElement(1710))),
 
+				new FC3ReadRegistersTask(1449, Priority.HIGH,
+						m(HeatpumpIdm.ChannelId.FLOW_TEMPERATURE_SETPOINT,
+								new UnsignedWordElement(1449))),
+
 				// Write Holding Registers
 				new FC16WriteRegistersTask(1393,
 						m(HeatpumpIdm.ChannelId.OPERATION_MODE,
